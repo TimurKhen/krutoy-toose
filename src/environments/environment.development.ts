e9137ced11f02a1a4397ed66e77e005ca83da122
Timur Khen <timurkhen10@gmail.com>
2026-07-16 00:28:45 +0300

    env update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://kringeproduction.ru',
+  apiUrl: 'https://www.kringeproduction.ru',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://kringeproduction.ru',
+  apiUrl: 'https://www.kringeproduction.ru',
 };
